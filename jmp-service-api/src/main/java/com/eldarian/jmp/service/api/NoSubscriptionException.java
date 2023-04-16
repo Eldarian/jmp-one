abd293750b4db2b494d013109682ac625dae6bbb
Dmitry_Plyshevsky <eldarian3@gmail.com>
2023-04-16 23:19:33 +0200

    fit and finish

--- jmp-service-api/src/main/java/com/eldarian/jmp/service/api/NoSubscriptionException.java
+++ jmp-service-api/src/main/java/com/eldarian/jmp/service/api/NoSubscriptionException.java
@@ -7,16 +7,4 @@ public class NoSubscriptionException extends Exception {
     public NoSubscriptionException(String message) {
         super(message);
     }
-
-    public NoSubscriptionException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    public NoSubscriptionException(Throwable cause) {
-        super(cause);
-    }
-
-    public NoSubscriptionException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-        super(message, cause, enableSuppression, writableStackTrace);
-    }
 }
